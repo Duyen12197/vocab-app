b5b49d3b68828b1e90b7dc265065c605bf644488
Duyen12197 <168201046+Duyen12197@users.noreply.github.com>
2025-12-30 08:35:56 +0700

    Update service worker cache configuration

--- sw.js
+++ sw.js
@@ -1,22 +1,10 @@
-const CACHE_NAME = "vocab-app-v1";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json"
-];
+const CACHE_NAME = 'eng-learn-v1';
+const assets = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(
-      response => response || fetch(event.request)
-    )
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
